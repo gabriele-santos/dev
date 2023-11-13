1d75c75cf71d6a89748bcc2827c129be189947f3
gabriele santos <gabbisn5@gmail.com>
2023-11-13 13:39:11 -0300

    Criando o componente Main para renderizar componentes principais

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import MainTitle from '../forMain/MainTitle/MainTitle';
+import AboutMe from '../forMain/AboutMe/AboutMe';
+function Main (){
+    return (
+        <main>
+        <MainTitle/>
+        <AboutMe/>
+        </main>
+    )
+}
+
+export default Main;
