39fe555f6ae64657c450d5eab5ed34c3e9791307
gabriele santos <gabbisn5@gmail.com>
2023-11-13 13:38:41 -0300

    Criando o componente MainTitle para título do projeto com imagem

---
+++ src/components/forMain/MainTitle/MainTitle.jsx
@@ -0,0 +1,32 @@
+import coding from './coding.gif';
+import './mainTitle.css';
+function MainTitle() {
+    return (
+        <div className='div-title-main'>
+            <div className='title-image'>
+                <div className='title'>
+                    <h2>
+                        Olá{''}
+                        <span className="hand" role="img" aria-labelledby="hand">
+                            👋🏻
+                        </span>, sou
+                    </h2>
+                    <br />
+                    <h1 style={{ color: '#F15A59' }}>Gabriele Santos</h1>
+                    <br />
+                    <h2>
+                        Uma desenvolvedora Front-End
+                    </h2>
+                    <div className='subtitle'>
+                        <p>Meu foco é criar sites intuitivos, responsivos e esteticamente atraentes!</p>
+                    </div>
+                </div>
+                <div>
+                    <img id='image-programmer' src={coding} alt="drawing of a programmer" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MainTitle;
