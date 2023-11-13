6ed37f98551d24ea0e554e50ab046cfc450d98bd
gabriele santos <gabbisn5@gmail.com>
2023-11-13 13:36:03 -0300

    Adicionando imagem gif à main

---
+++ src/components/Images/ImageTitle.jsx
@@ -0,0 +1,10 @@
+import coding from '../Images/coding.gif';
+import './imageTitle.css';
+
+function ImageTitle(){
+    return (
+        <img id='image-programmer' src={coding} alt="drawing of a programmer" />
+    )
+}
+
+export default ImageTitle;
