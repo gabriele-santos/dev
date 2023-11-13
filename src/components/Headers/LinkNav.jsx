16657d25441e412449c2444231c25d8f0c9be1a5
gabriele santos <gabbisn5@gmail.com>
2023-11-13 17:51:40 -0300

    Colocando valores booleanos de acordo com o Eslint

--- src/components/Headers/LinkNav.jsx
+++ src/components/Headers/LinkNav.jsx
@@ -9,16 +9,16 @@ function LinkNav() {
         <img src={cloud} alt="computer in icon format" style={{ width: "50px" }} />
       </div>
       <div className="links">
-        <Link activeClass="active" to="sobre" offset={-200} className="link-header">
+        <Link activeClass="active" to="sobre" spy smooth offset={-200} className="link-header">
           Sobre
         </Link>
-        <Link activeClass="active" to="contato" offset={-200} className="link-header">
+        <Link activeClass="active" to="contato" spy smooth offset={-200} className="link-header">
           Contato
         </Link>
-        <Link activeClass="active" to="habilidades" offset={-200} className="link-header">
+        <Link activeClass="active" to="habilidades" spy smooth offset={-200} className="link-header">
           Habilidades
         </Link>
-        <Link activeClass="active" to="projetos" className="link-header">
+        <Link activeClass="active" to="projetos" spy smooth offset={-200} className="link-header">
           Projetos
         </Link>
       </div>
