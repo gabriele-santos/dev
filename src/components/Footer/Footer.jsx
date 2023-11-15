bf20ae857069d4e4c916303a0470a657cf869ce6
gabriele santos <gabbisn5@gmail.com>
2023-11-14 22:29:34 -0300

    Adicionando o componente footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-div">
+      <p id="footer-id">Desenvolvido por Gabriele Santos</p>
+    </div>
+  );
+}
+
+export default Footer;
