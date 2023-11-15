e8e90688fb6431f15c793c0120d1868a303b37b2
gabriele santos <gabbisn5@gmail.com>
2023-11-14 22:21:10 -0300

    Adicionando e estilizando os links de navegação

--- src/components/Headers/LinkNav.jsx
+++ src/components/Headers/LinkNav.jsx
@@ -1,5 +1,9 @@
 import { Link } from "react-scroll";
-import cloud from "./cloud.svg";
+import cloud from "./svg/cloud.svg";
+import person from "./svg/person.svg";
+import contact from "./svg/website.svg";
+import ability from "./svg/ability.svg";
+import project from "./svg/project.svg";
 import "./linkNav.css";
 
 function LinkNav() {
@@ -9,17 +13,21 @@ function LinkNav() {
         <img src={cloud} alt="computer in icon format" style={{ width: "50px" }} />
       </div>
       <div className="links">
-        <Link activeClass="active" to="sobre" spy smooth offset={-200} className="link-header">
-          Sobre
+        <Link activeClass="active" to="about" spy smooth offset={-200} className="link-header">
+          <img className="svg-nav" src={person} style={{ width: "20px" }} alt="icon person" />
+          <span>Sobre</span>
         </Link>
-        <Link activeClass="active" to="contato" spy smooth offset={-200} className="link-header">
-          Contato
+        <Link activeClass="active" to="contact" spy smooth offset={-200} className="link-header">
+          <img className="svg-nav" src={contact} style={{ width: "20px" }} alt="icon contact" />
+          <span> Contato</span>
         </Link>
-        <Link activeClass="active" to="habilidades" spy smooth offset={-200} className="link-header">
-          Habilidades
+        <Link activeClass="active" to="skills" spy smooth offset={-200} className="link-header">
+          <img className="svg-nav" src={ability} style={{ width: "20px" }} alt="icon ability" />
+          <span>Habilidades</span>
         </Link>
-        <Link activeClass="active" to="projetos" spy smooth offset={-200} className="link-header">
-          Projetos
+        <Link activeClass="active" to="projects" spy smooth offset={-200} className="link-header">
+          <img className="svg-nav" src={project} style={{ width: "20px" }} alt="icon project" />
+          <span>Projetos</span>
         </Link>
       </div>
     </header>
