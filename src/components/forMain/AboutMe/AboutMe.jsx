ff3db0a3300c4d46c65528c8af8ee6a8c16414ab
gabriele santos <gabbisn5@gmail.com>
2023-11-14 22:26:19 -0300

    Adicionando o componente sobre mim

--- src/components/forMain/AboutMe/AboutMe.jsx
+++ src/components/forMain/AboutMe/AboutMe.jsx
@@ -1,17 +1,36 @@
+import minhaFoto from "../../Images/minhaFoto.png";
+import IconContacts from "../Contact/IconContacts";
+import "./aboutMe.css";
+
 function AboutMe() {
   return (
-    <div className="sobre">
-      <h1>Sobre mim</h1>
-      <p>
-        Sou uma desenvolvedora Front-End apaixonada por transformar ideias em realidade
-        digital.
-      </p>
-      <p>
-        Tenho uma visão centrada no usuário, buscando constantemente aprimorar minhas habilidades
-        e conhecimentos, assim como a usabilidade e a acessibilidade em cada projeto, com o objetivo
-        de me tornar uma profissional melhor a cada dia.
-      </p>
-    </div>
+    <section id="about">
+      <div className="text-and-image">
+        <div className="my-image">
+          <img src={minhaFoto} alt="uma foto minha" style={{ width: "400px" }} />
+        </div>
+        <div className="container-text">
+          <div className="title-paragraph">
+            <h1 style={{ color: "#F15A59" }}>Sobre mim</h1>
+          </div>
+          <div className="paragraph">
+            <p>
+              Sou uma desenvolvedora
+              <span style={{ color: "#F15A59" }}> Front-End </span>
+              apaixonada por transformar ideias em realidade
+              digital.
+              <br />
+              Tenho uma visão centrada no usuário, buscando constantemente aprimorar minhas
+              habilidades e conhecimentos, assim como a usabilidade e a acessibilidade em cada
+              projeto, com o objetivo de me tornar uma profissional melhor a cada dia.
+            </p>
+          </div>
+          <div className="icon-to-contact">
+            <IconContacts />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
