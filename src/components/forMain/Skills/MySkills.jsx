46a49d91aa460e5d905699d25bb132ba194ad2b4
gabriele-santos <gabbisn5@gmail.com>
2024-05-23 18:33:32 -0300

    Adicionando swiper de habilidades

--- src/components/forMain/Skills/MySkills.jsx
+++ src/components/forMain/Skills/MySkills.jsx
@@ -1,5 +1,10 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "./mySkills.css";
 import skills from "./skills.gif";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
 function MySkills() {
   return (
@@ -8,26 +13,27 @@ function MySkills() {
         <h1 className="centered-title" style={{ color: "#F15A59" }}>Minhas Habilidades</h1>
       </div>
       <div className="container-no-title">
-        <div className="flex-container">
-          <div className="div-image-skills">
-            <img id="image-skills" src={skills} alt="my skills" />
-          </div>
-          <div className="container-text-skills">
-            <div id="skills">
-              <p>JavaScript</p>
-              <p>HTML</p>
-              <p>CSS</p>
-              <p>React</p>
-              <p>TypeScript</p>
-              <p>Git</p>
-              <p>GitHub</p>
-              <p>Jest</p>
-            </div>
-          </div>
+        <div className="div-image-skills">
+          <img id="image-skills" src={skills} alt="my skills" />
         </div>
+        <Swiper
+          className='swiper-container'
+          modules={[Navigation, Pagination, Autoplay]}
+          autoplay={{ delay: 1000 }}
+          navigation
+          pagination={{ clickable: true }}
+        >
+          <SwiperSlide className="swiper-item">JavaScript</SwiperSlide>
+          <SwiperSlide className="swiper-item">HTML</SwiperSlide>
+          <SwiperSlide className="swiper-item">CSS</SwiperSlide>
+          <SwiperSlide className="swiper-item">React</SwiperSlide>
+          <SwiperSlide className="swiper-item">TypeScript</SwiperSlide>
+          <SwiperSlide className="swiper-item">Git</SwiperSlide>
+          <SwiperSlide className="swiper-item">GitHub</SwiperSlide>
+          <SwiperSlide className="swiper-item">Jest</SwiperSlide>
+        </Swiper>
       </div>
     </section>
-
   );
 }
 
