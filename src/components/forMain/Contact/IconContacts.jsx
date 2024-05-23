82b040a47ed6e296367f2d7d0d13ce79bcfd0395
gabriele-santos <gabbisn5@gmail.com>
2024-05-23 18:59:51 -0300

    Adicionando links perfis profissionais

--- src/components/forMain/Contact/IconContacts.jsx
+++ src/components/forMain/Contact/IconContacts.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-scroll";
 import "./iconContacts.css";
 import gitHub from "./svg/github.svg";
 import linkedin from "./svg/linkedin.svg";
@@ -5,18 +6,18 @@ import linkedin from "./svg/linkedin.svg";
 function IconContacts() {
   return (
     <div id="contact">
-      <div>
-        <img style={{ width: "30px" }} src={gitHub} alt="" />
+      <a href="https://github.com/gabriele-santos" target="_blank" className="link">
+        <img style={{ width: "30px" }} src={gitHub} alt="github" />
         <span className="link-contact link-github">
           GitHub
         </span>
-      </div>
-      <div>
-        <img style={{ width: "29px" }} src={linkedin} alt="" />
+      </a>
+      <a href="https://www.linkedin.com/in/gabriele-santos-nc/" target="_blank" className="link">
+        <img style={{ width: "29px" }} src={linkedin} alt="linkedin" />
         <span className="link-contact">
           LinkedIn
         </span>
-      </div>
+      </a>
     </div>
   );
 }
