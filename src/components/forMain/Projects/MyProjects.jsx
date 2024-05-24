df599a76c38aa4e47b7546d7ab12578b1a5d2920
gabriele-santos <gabbisn5@gmail.com>
2024-05-23 22:21:58 -0300

    Adicionando mais projetos

--- src/components/forMain/Projects/MyProjects.jsx
+++ src/components/forMain/Projects/MyProjects.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import "./myProjects.css";
 import imageProjects from "./projects.gif";
 
 function MyProjects() {
+
+  const [showListProjects, setShowListProjects] = useState(false);
+
   return (
     <section className="projects">
       <div className="centered-title-projects">
@@ -11,9 +15,28 @@ function MyProjects() {
         <div className="paragraph-project">
           <h3 className="text-projects" href="link">Portfólio Web</h3>
           <p className="text-projects">Esta página em que você está navegando é resultado de um dos projetos que desenvolvi.</p>
-          <a id="link-portfolio" className="text-projects" href="link">Link para o repositório</a>
+          <a id="link-portfolio" className="text-projects" href="https://github.com/gabriele-santos/dev" target="_blank">Link para o repositório
+          </a>
           <hr />
-          <h4 className="all-projects">Ver todos os projetos...</h4>
+          {showListProjects === false && (<h4 className="all-projects" onClick={() => setShowListProjects(true)}>Ver todos os projetos...</h4>)}
+        {showListProjects && (
+          <div className="list-projects">
+            <div>
+              <h3 className="text-projects" href="link">Formulário React</h3>
+              <p className="text-projects">Esse projeto consiste em um formulário de cadastro de currículo com informações pessoais e profisisonais.</p>
+              <a id="link-portfolio" className="text-projects" href="https://github.com/gabriele-santos/Formulario-React" target="_blank">Link para o repositório
+              </a>
+              <hr />
+            </div>
+            <div className="list-projects">
+              <h3 className="text-projects" href="link">Form Travel</h3>
+              <p className="text-projects">Esse projeto consiste em um formulário de viagens.</p>
+              <a id="link-portfolio" className="text-projects" href="https://github.com/gabriele-santos/Form_Travel" target="_blank">Link para o repositório
+              </a>
+              <hr />
+            </div>
+          </div>
+        )}
         </div>
         <div className="flex-text-image image-projects">
           <img src={imageProjects} alt="developing my projects" />
